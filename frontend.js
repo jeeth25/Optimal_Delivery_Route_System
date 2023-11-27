14bd401c486f7d62770c9bc583842629f51e8c16
Jahanvipaliwal0 <jahanvipaliwal0@csu.fullerotn.edu>
2023-11-26 22:49:04 -0800

    first commit

---
+++ frontend.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './App.css';
+import 'index.html';
+
+function App() {
+ const [deliveryLocations, setDeliveryLocations] = useState([]);
+
+ const handleAddLocation = (event) => {
+    event.preventDefault();
+    setDeliveryLocations([...deliveryLocations, { address: event.target.address.value }]);
+    event.target.address.value = '';
+ };
+
+ const handleSubmit = async (event) => {
+    event.preventDefault();
+    // Call the backend to execute the TSP algorithm
+    const response = await fetch('/tsp', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(deliveryLocations),
+    });
+
+    const data = await response.json();
+    console.log('Optimized Route:', data);
+ };
+
+ return (
+    <div className="App">
+      <form onSubmit={handleAddLocation}>
+        <label>
+          Delivery Location:
+          <input type="text" name="address" required />
+        </label>
+        <button type="submit">Add Location</button>
+      </form>
+
+      <form onSubmit={handleSubmit}>
+        <h2>Delivery Locations:</h2>
+        <ul>
+          {deliveryLocations.map((location, index) => (
+            <li key={index}>{location.address}</li>
+          ))}
+        </ul>
+        <button type="submit">Optimize Route</button>
+      </form>
+    </div>
+ );
+}
+
+export default App;
