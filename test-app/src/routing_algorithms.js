77bc3f1ca554a5553b22f0013aadb6ea1603e0f1
Jeet Hirakani <51905899+jeeth25@users.noreply.github.com>
2023-11-29 21:17:43 -0800

    removed some useless code

--- test-app/src/routing_algorithms.js
+++ test-app/src/routing_algorithms.js
@@ -1,37 +1,3 @@
-function calculateDistance(city1, city2) {
-  const { lat: lat1, lon: lon1 } = city1;
-  const { lat: lat2, lon: lon2 } = city2;
-  const radius = 6371;  // Radius of the Earth in kilometers
-  const dlat = toRadians(lat2 - lat1);
-  const dlon = toRadians(lon2 - lon1);
-
-  let a = Math.sin(dlat / 2) ** 2 + Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) * Math.sin(dlon / 2) ** 2;
-  a = Math.min(1, a);  // Ensure a is within the valid range [0, 1]
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = radius * c;
-
-  return distance;
-}
-
-function toRadians(degrees) {
-  return degrees * (Math.PI / 180);
-}
-
-function getDistanceMatrix(cities) {
-  const numCities = cities.length;
-  const distances = new Array(numCities).fill(0).map(() => new Array(numCities).fill(0));
-
-  for (let i = 0; i < numCities; i++) {
-      for (let j = i + 1; j < numCities; j++) {
-          const dist = calculateDistance(cities[i], cities[j]);
-          distances[i][j] = Math.round(dist);
-          distances[j][i] = Math.round(dist);  // Ensure symmetry
-      }
-  }
-
-  return distances;
-}
-
 export const solveTSPNearest = (distances) => {
     const numCities = distances.length;
     const visited = new Array(numCities).fill(false);
@@ -208,31 +174,3 @@ function generateAllPermutations(n) {
   permute(indices, 0, n - 1);
   return permutations;
 }
-
-const cities = [
-  { "lat": 52.5200, "lon": 13.4050 },
-  { "lat": 53.5511, "lon": 9.9937 },
-  { "lat": 48.1351, "lon": 11.5820 },
-  { "lat": 50.9375, "lon": 6.9603 },
-  { "lat": 50.1109, "lon": 8.6821 },
-];
-
-const distances = getDistanceMatrix(cities);
-
-distances.forEach(row => {
-  console.log(row);
-});
-
-const { tour: nearestTour, totalDistance: nearestTotalDistance } = solveTSPNearest(distances);
-const { tour: bruteForceTour, totalDistance: bruteForceTotalDistance } = solveTSPBruteForce(distances);
-const { tour: mstTour, totalDistance: mstTotalDistance } = solveTSPMST(distances);
-
-console.log("Nearest Neighbor Tour:", nearestTour);
-console.log("Nearest Neighbor Total distance:", nearestTotalDistance);
-
-console.log("Brute Force Tour:", bruteForceTour);
-console.log("Brute Force Total distance:", bruteForceTotalDistance);
-
-console.log("Minimum Spanning Tree Tour:", mstTour);
-console.log("Minimum Spanning Tree Total distance:", mstTotalDistance);
-
